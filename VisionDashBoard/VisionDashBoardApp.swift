8989dca9f1fda203b3aba76de6183a3222169c50
zhengjie <jerryxuzhengjie@gmail.com>
2024-04-29 09:57:46 +0800

    Initial Commit

---
+++ VisionDashBoard/VisionDashBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionDashBoardApp.swift
+//  VisionDashBoard
+//
+//  Created by 徐政杰 on 2024/4/29.
+//
+
+import SwiftUI
+
+@main
+struct VisionDashBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
