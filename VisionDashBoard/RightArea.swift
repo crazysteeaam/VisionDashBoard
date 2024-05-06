9f31c7e96b877a9471cb40511d11fa9fcf118868
zhengjie <jerryxuzhengjie@gmail.com>
2024-05-06 12:00:11 +0800

    Support more components

---
+++ VisionDashBoard/RightArea.swift
@@ -0,0 +1,20 @@
+//
+//  RightArea.swift
+//  VisionDashBoard
+//
+//  Created by 徐政杰 on 2024/5/6.
+//
+
+import SwiftUI
+
+struct RightArea: View {
+    var body: some View {
+        TaskView()
+        TaskView()
+    }
+}
+
+
+#Preview(windowStyle: .automatic) {
+    RightArea()
+}
